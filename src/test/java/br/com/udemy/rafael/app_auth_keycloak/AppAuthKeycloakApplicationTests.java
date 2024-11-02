11be95cbe94b90c949e3b89d1e086c5feeba9c0d
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-11-02 16:01:45 -0300

    feat: first commit

---
+++ src/test/java/br/com/udemy/rafael/app_auth_keycloak/AppAuthKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.rafael.app_auth_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAuthKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
