7eeae097879b598375aeecd1e749876a47c11a64
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-11-03 18:19:34 -0300

    feat: add config jwt converter with oauth2

--- src/main/java/br/com/udemy/rafael/app_auth_keycloak/security/config/SecurityConfig.java
+++ src/main/java/br/com/udemy/rafael/app_auth_keycloak/security/config/SecurityConfig.java
@@ -2,16 +2,25 @@ package br.com.udemy.rafael.app_auth_keycloak.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+//    ROLE_ADMIN
+//    ROLE_USER
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())));
+
         return http.build();
     }
 }
