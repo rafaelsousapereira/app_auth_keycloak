371c996a7fdb9938d1dfcf90a19b454045238e7d
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-11-03 18:20:30 -0300

    feat: add preauthorize roles

--- src/main/java/br/com/udemy/rafael/app_auth_keycloak/controller/ProductController.java
+++ src/main/java/br/com/udemy/rafael/app_auth_keycloak/controller/ProductController.java
@@ -1,5 +1,6 @@
 package br.com.udemy.rafael.app_auth_keycloak.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @GetMapping(value = "/")
+    @PreAuthorize("hasRole('USER')")
     public String listProducts() {
         return "Product list";
     }
 
     @PostMapping(value = "/")
+    @PreAuthorize("hasRole('ADMIN')")
     public String createProduct() {
         return "Product create";
     }
